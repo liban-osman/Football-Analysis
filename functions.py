d79ecedd2789b25d6fb45cc1691d6549aa39d237
Liban Osman <libanosman108@gmail.com>
2022-08-24 18:51:04 -0400

    pushing first files

---
+++ functions.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import json
+
+def load_data(file_path):
+    '''
+    Takes in the file_path of the JSON event and returns the matchCentreData
+    '''
+    with open(file_path, 'r') as j:
+        contents = json.loads(j.read())
+    data = contents["matchCentreData"]
+    return data
+
+def players_list(data):
+    players = pd.DataFrame.from_dict(data["playerIdNameDictionary"],  orient='index')
+    players = players.reset_index()
+    players.columns = ["players_id", "player_name"]
+    return players
+
